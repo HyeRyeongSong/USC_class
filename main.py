e17dd56e01a5295e5f697cbe2e5f691df08dcc61
=HyeRyeongSong <=loverhdiddl@gmail.com>
2019-07-01 23:56:12 -0700

    Lab 1: Reading CSV file #1

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-import tensorflow as tf
+# read “csv” files using the “csv” package
+import csv
+# perform sentiment analysis using the “TextBlob” package
+from textblob import TextBlob
+
+# use the “open()” function to open a specific file
+# include the “encoding=‘UTF-8’ “ to read special characters
+with open('Data.csv', encoding="UTF-8") as csv_file:
+    # use the “csv.reader()” because we are reading a CSV file
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    # The “for row in csv_reader:” makes the code iterate over all data in the csv file
+    # Each row has a Data Type “List”
+    # Each list will have 2 elements since we have 2 values(ID and Text) in the CSV file
+    # Row[0] will give the ID and Row[1] will give the Text.
+    for row in csv_reader:
+        print (row[0])
+        print (row[1])
+
 
