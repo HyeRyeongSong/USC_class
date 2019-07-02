c092df3bbe65434e85ecc607c5cae270f3c3c785
=HyeRyeongSong <=loverhdiddl@gmail.com>
2019-07-02 00:04:07 -0700

    Lab 1: Reading CSV file #2

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ import csv
 from textblob import TextBlob
 
 # use the “open()” function to open a specific file
-# include the “encoding=‘UTF-8’ “ to read special characters
+# include the “encoding=‘UTF-8’“ to read special characters
 with open('Data.csv', encoding="UTF-8") as csv_file:
     # use the “csv.reader()” because we are reading a CSV file
     csv_reader = csv.reader(csv_file, delimiter=',')
+    # use a Integer variable “line_count” to keep track of whether the data is a header or not
+    line_count = 0
     # The “for row in csv_reader:” makes the code iterate over all data in the csv file
-    # Each row has a Data Type “List”
-    # Each list will have 2 elements since we have 2 values(ID and Text) in the CSV file
-    # Row[0] will give the ID and Row[1] will give the Text.
     for row in csv_reader:
-        print (row[0])
-        print (row[1])
+        # Only the first row in the CSV file is the header information
+        # so we exclude the first row and start printing from the second row
+        if line_count == 0:
+            line_count = line_count + 1
+
+        # Only the text will be printed because we called “row[1]” and we do not need the ID information
+        else:
+            txt = row[1]
+            print (txt)
 
 
