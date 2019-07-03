768e91cf776307433722f5e8c6a7a6ef0ef09b42
=HyeRyeongSong <=loverhdiddl@gmail.com>
2019-07-03 14:01:19 -0700

    Lab 3-1: GeoPy - GeoCoding Eample

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
-import csv
-import matplotlib.pyplot as plt
+from geopy.geocoders import Nominatim
 
-## Visualize Word Count
+# Create A GeoLocator Object
+geolocator = Nominatim(user_agent="HyeRyeong_Song_app")
 
-# Reading CSV File into a dictionary
-final_dictionary = {}
+# To geolocate a query to an address
+location = geolocator.geocode("175 5th Avenue NYC", addressdetails=True)
 
-with open('wordCount.csv', mode='r', encoding='UTF-8') as my_file:
-    csv_reader = csv.reader(my_file, delimiter=',')
-    line_count = 0
+print(location.address)
 
-    for row in csv_reader:
-        line_count = line_count + 1
+print((location.latitude, location.longitude))
 
-        if line_count == 1:
-            continue;
-
-        word = str(row[0])
-        count = int(row[1])
-
-        final_dictionary[word] = count
-        print(word + "\t" + str(count))
-
-        if line_count == 21:
-            break;
-
-# Plot Word Count
-plt.figure(figsize=(15, 3))
-
-plt.title('Frequency of Most Frequent Words')
-plt.xlabel('Most Frequent Words')
-plt.ylabel('Frequency')
-
-plt.bar(final_dictionary.keys(), final_dictionary.values(), width=0.3, color='g')
-
-plt.show()
+print(location.raw)
